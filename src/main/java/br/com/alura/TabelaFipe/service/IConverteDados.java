a1650a7093ab8e82f2a6cbfb26304d2e1c0ab16a
David <davidfl44.pro@gmail.com>
2024-12-01 11:02:23 -0300

    Projeto Tabela Fipe

---
+++ src/main/java/br/com/alura/TabelaFipe/service/IConverteDados.java
@@ -0,0 +1,14 @@
+package br.com.alura.TabelaFipe.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.List;
+
+public interface IConverteDados {
+    <T> T obterDados(String json, Class<T> classe);
+
+    <T> List<T> obterLista(String json, Class<T> classe);
+
+}
+
